840b83655717caee113ec3e8983d5b2786e4221c
Jia Cui <cuijiajanae@gmail.com>
2021-08-02 15:26:42 -0700

    fixed some bugs

--- react-app/src/components/CreateJobFormModal/index.js
+++ react-app/src/components/CreateJobFormModal/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Modal } from "../../context/Modal";
 import CreateJobForm from "./JobFormModal";
 import styles from "../../components/CreateJobFormModal/JobFormModal.module.css"
@@ -8,10 +9,10 @@ import { useSelectedBoard } from '../../context/SelectedBoard';
 function JobFormModal() {
   const [showModal, setShowModal] = useState(false);
   const {selected, setSelected}= useSelectedBoard();
-
+  const boards = useSelector((state) => Object.values(state.boards));
   return (
     <>
-      <UilPlus display={selected? "display" : "none"}  className={styles.dashBoardBigPlusButton}
+      <UilPlus display={selected && boards.length > 0 ?  "display" : "none"}  className={styles.dashBoardBigPlusButton}
                onClick={() => setShowModal(true)}/>
       {showModal && (
         <Modal onClose={() => setShowModal(false)}>
